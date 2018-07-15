5e66c3245d8e4c4b8f744ca207add1a82bd1a675
Ian <bennetti@mail.uoguelph.ca>
2018-07-15 18:09:29 -0400

    Added menubar and social share

--- js/modal.js
+++ js/modal.js
@@ -3,16 +3,46 @@ $( document ).ready(function() {
 
 	var span = document.getElementsByClassName("close")[0];
 
+	var showMeOff2 = document.getElementById('sharemodal');
+
+	var span2 = document.getElementsByClassName("close")[1];
 
 
 span.onclick = function() {
-                showMeOff.style.display = "none";
+	try{
+		showMeOff.style.display = "none";
+	}
+	catch {
+	}
+
+	try{
+		 showMeOff2.style.display = "none";
+	}
+	catch {	
+	}                               
 }
 
 window.onclick = function(event) {
-    if (event.target == showMeOff) {
-                showMeOff.style.display = "none";
+    if (event.target == showMeOff || event.target == showMeOff2) {
+
+    	try{
+    		showMeOff.style.display = "none";
+    	}
+    	catch{
+    	}
+
+    	try{
+    		showMeOff2.style.display = "none";
+    	}
+    	catch{
+    	}             
     }
 }
 
 });
+
+
+function openPopup(url) {
+       newwindow=window.open(url,'Social Share','height=400,width=750');
+       if (window.focus) {newwindow.focus()}
+     }
